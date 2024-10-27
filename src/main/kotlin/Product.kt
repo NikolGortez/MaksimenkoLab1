3b2018ba9a1d085ddb0c827ed36f48077d8accee
NikolGortez <nikolgortez@yandex.ru>
2024-10-27 20:44:54 +0500

    3. полный класс сущности `product` с инкапсуляцией
    Инкапсуляция в Kotlin скрыта за синтаксисом языка, обычный доступ
    к полям через оператор `.` на самом деле в байт-коде вызывает
    геттеры и сеттеры.

---
+++ src/main/kotlin/Product.kt
@@ -0,0 +1,10 @@
+package voidpointer.mc.tests
+
+data class Product(
+    val id : Int,
+    val name : String,
+    val description : String,
+    val price : Double,
+    val stockBalance : Int,
+    val reserveBalance : Int,
+)
